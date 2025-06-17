97f7cde996b0f1b8cc45eafeb90886ec44962320
Phil-Mure <74577387+Phil-Mure@users.noreply.github.com>
2025-06-17 15:04:11 +0200

    Update agents.py

--- agents.py
+++ agents.py
@@ -302,12 +302,12 @@ from langgraph.prebuilt import create_react_agent
 # and general purpose websearch.---
 
 def KnowledgeAgent(input_message: str) -> str:
-    #res = ""
-    #if classify_user_input_with_labels(labels, input_message) == "No Text Found":
-       # res += #summarize_websearch_results(web_search_tool(input_message)).content 
+    res = ""
+    if classify_user_input_with_labels(labels, input_message) == "No Text Found":
+        res += summarize_websearch_results(web_search_tool(input_message)).content 
     
-    #else:
-    res = list(graph.stream({"messages": [{"role": "user", "content": input_message}]}, stream_mode="values",
+    else:
+        res += list(graph.stream({"messages": [{"role": "user", "content": input_message}]}, stream_mode="values",
             config=config,
         ))[-1]["messages"][-1].content
     print(res)
@@ -403,7 +403,7 @@ def router(user_input: str, user_id: int) -> str:
         return {
             "response": PersonalityLayer(KnowledgeAgent(user_input)),
             "source_agent_response": KnowledgeAgent(user_input),
-            "agent_workflow": [{"agent_name": "Knowledge Agent", "tool_calls": {"WebSearchTool": "None"}}]
+            "agent_workflow": [{"agent_name": "Knowledge Agent", "tool_calls": {"WebSearchTool": web_search_tool(user_input)}}]
         }
     elif decision == "B":
         source_agent_response = CustomerAgent(user_input, user_id)
